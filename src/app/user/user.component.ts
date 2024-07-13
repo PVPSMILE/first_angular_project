11676107b684f3dc84d499a99a589b64f1b2b771
Artem <olehpopkov@MacBook-Pro-Oleh.local>
2024-07-13 14:11:27 +0300

    37. Exercise: Create a Configurable Component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, Output, EventEmitter } from '@angular/core';
+import { Component, computed, Input, Output, output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,7 +11,8 @@ export class UserComponent {
   @Input({required: true} ) id!: string;
   @Input({required: true} ) avatar!: string;
   @Input({required: true} ) name!: string;
-  @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter<string>();
+  
   get imagePath(){
     return 'assets/users/' + this.avatar;
   
