966d3a7fdbd3460d1dd3bae275d1214927a31b1a
Artem <olehpopkov@MacBook-Pro-Oleh.local>
2024-07-15 13:12:39 +0300

    47. Storing Data Models in Separate Files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,12 @@ import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from './tasks/tasks.component';
-import { NgFor, NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -19,7 +19,6 @@ export class AppComponent {
   selectedUserId?: string;
 
   get selectedUser() {
-
       return (this.users.find(user => user.id === this.selectedUserId));
   }
   get selectedUserid() {
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,6 @@
 import { Component, computed, Input, Output, output, EventEmitter } from '@angular/core';
 
-type User = {
-  id: string;
-  name: string;
-  avatar: string;
-}
+import { type User } from './user.modal';
 
 @Component({
   selector: 'app-user',
